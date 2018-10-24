09ec35e4d6b32bee37ad38e955b06c42d8ffbd1c
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-10-24 09:33:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-function happyHolidays = 
+function happyHolidays () {
+  console.log("Happy holidays!")
+}
+function happyHolidaysTo (name) {
+  return `Happy holidays, ${name}!` 
+}
+function happyHolidayTo (holiday, name) {
+  return `Happy ${holiday}, ${name}!`
+}
